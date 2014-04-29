912c031e012c7f395cd3db4ce4227e0294459682
Svetlana <lumenela@gmail.com>
2014-04-29 15:13:36 +0300

    Added page for search in XML

---
+++ Controllers/XMLSearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Critters.Controllers
+{
+    public class XMLSearchController : Controller
+    {
+        //
+        // GET: /XMLSearch/
+        public ActionResult Index(string searchString)
+        {
+            return View();
+        }
+
+    }
+}
