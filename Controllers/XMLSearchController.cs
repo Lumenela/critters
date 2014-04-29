877be4b53ff07ac89ef07ee6673ab7fdce8e5be0
Svetlana <lumenela@gmail.com>
2014-04-29 16:30:03 +0300

    Showing xml content

--- Controllers/XMLSearchController.cs
+++ Controllers/XMLSearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace Critters.Controllers
 {
@@ -12,6 +13,23 @@ namespace Critters.Controllers
         // GET: /XMLSearch/
         public ActionResult Index(string searchString)
         {
+            string path = HttpContext.Server.MapPath("~/App_Data/books.xml");
+            XElement root = XElement.Load(path);
+            if (String.IsNullOrEmpty(searchString))
+            {
+                ViewBag.XmlContainer = root;
+            }
+            else
+            {
+             /*   string text = "";
+                IEnumerable<XElement> books =
+                from el in root.Elements("book")
+                select el;
+                foreach (XElement el in books)
+                    text += "\n" + el;*/
+            }
+            
+            
             return View();
         }
 
